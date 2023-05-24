e9d16376e3b88b1faece98409cee265c4880e99b
PierreTembremande <pierre.tembremande@gmail.com>
2023-05-24 09:39:29 +0200

    mise en place de la zone d'erreur

--- src/routes/Formulaire.js
+++ src/routes/Formulaire.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
+let contientToutChamp = [];
 let recup = [];
+let tableauErreur = [];
+const regex = /[123456789]/
+let erreurAlpha = false;
+let erreurChampRequis = false;
 
 export default function CreationFormulaire(information) {
+
+    const [erreur, SetErreur] = useState(false);
+    const [affichageErreur, SetAffichageErreur] = useState([]);
 
     information = [{
         "id": "customer",
@@ -59,27 +67,45 @@ export default function CreationFormulaire(information) {
         ]
     }]
 
-    function gestionChamp(donnee) {
+    function gestionChamp(donnee, i) {
 
         let contientChamp = [];
 
-        if (donnee.type == "text" || donnee.type == "number" || donnee.type == "radio" || donnee.type == "checkbox") {
-            contientChamp.push(<input hidden={visible(donnee.visibility, donnee.id)} key={"input" & donnee.id} id={donnee.id} type={donnee.type} required={false} onChange={({ target: { value, id } }) => { visible2(value, id) }} ></input>);
+        if (donnee.type === "text" || donnee.type === "number" || donnee.type === "radio" || donnee.type === "checkbox") {
+            contientChamp.push(<div key={"input" & donnee.id} hidden={visible(donnee.visibility, donnee.id + i)} id={donnee.id + i}>
+                <label key={"label" + i} id={"label" + donnee.id}>{donnee.label}</label>
+                <input id={donnee.id} type={donnee.type} required={donnee.required} onChange={({ target: { value } }) => { visible2(value, donnee.id) }} ></input>
+                <p id={"valeurVide" + donnee.id} hidden={true} >Ce champ est requis</p>
+            </div>);
         }
 
-        if (donnee.type == "select") {
-            contientChamp.push(<select hidden={visible(donnee.visibility, donnee.id)} key={"select" & donnee.id} id={donnee.id} onChange={({ target: { value, id } }) => { visible2(value, id) }}>
-                {donnee.option.map((contenu, i) => {
-                    return (
-                        <option key={i} value={contenu}>{contenu}</option>
-                    )
-                })}
-            </ select>)
+        if (donnee.type === "select") {
+            contientChamp.push(<div key={"select" & donnee.id} hidden={visible(donnee.visibility, donnee.id + i)} id={donnee.id + i}>
+                <label key={"label" + i} id={"label" + donnee.id}>{donnee.label}</label>
+                <select id={donnee.id} onChange={({ target: { value } }) => { visible2(value, donnee.id) }}>
+                    {donnee.option.map((contenu, i) => {
+                        return (
+                            <option key={i} value={contenu}>{contenu}</option>
+                        )
+                    })}
+                </ select>
+                <p id={"valeurVide" + donnee.id} hidden={true} >Ce champ est requis</p>
+            </div>)
         }
 
-        if (donnee.type == "textarea") {
-            contientChamp.push(<textarea hidden={visible(donnee.visibility, donnee.id)} key={"textarea" & donnee.id} id={donnee.id} required={false} onChange={({ target: { value, id } }) => { visible2(value, id) }}></textarea>);
+        if (donnee.type === "textarea") {
+            contientChamp.push(<div key={"textarea" & donnee.id} hidden={visible(donnee.visibility, donnee.id + i)} id={donnee.id + i}>
+                <label key={"label" + i} id={"label" + donnee.id}>{donnee.label}</label>
+                <textarea id={donnee.id} required={donnee.required} onChange={({ target: { value } }) => { visible2(value, donnee.id) }}></textarea>
+                <p id={"valeurVide" + donnee.id} hidden={true} >Ce champ est requis</p>
+            </div>);
         }
+
+        if (!contientToutChamp.includes(donnee.id)) {
+            contientToutChamp.push(donnee.id + "," + donnee.required);
+        }
+
+        contientToutChamp = contientToutChamp.filter((x, i) => contientToutChamp.indexOf(x) === i);
 
         return contientChamp;
     }
@@ -96,7 +122,6 @@ export default function CreationFormulaire(information) {
 
         let v = true;
         return v;
-
     }
 
     function visible2(valeur, id) {
@@ -146,32 +171,91 @@ export default function CreationFormulaire(information) {
                 }
             }
         }
-
     }
 
+    function donneeRequise() {
+        let champRequis = [];
+
+        for (let i = 0; i < contientToutChamp.length; i++) {
+            champRequis.push(contientToutChamp[i].split(","));
+            if (champRequis[i][1] === "true") {
+                if (document.getElementById(champRequis[i][0]).type === "checkbox" || document.getElementById(champRequis[i][0]).type === "radio") {
+                    if (!document.getElementById(champRequis[i][0]).checked) {
+                        document.getElementById("valeurVide" + champRequis[i][0]).hidden = false;
+                    } else {
+                        document.getElementById("valeurVide" + champRequis[i][0]).hidden = true;
+                    }
+                } else {
+                    if (document.getElementById(champRequis[i][0]).value === "") {
+                        document.getElementById("valeurVide" + champRequis[i][0]).hidden = false;
+                    } else {
+                        document.getElementById("valeurVide" + champRequis[i][0]).hidden = true;
+                    }
+                }
+            }
+        }
+    }
+
+    function verificationErreur() {
+        tableauErreur = [];
+        for (let i = 0; i < contientToutChamp.length; i++) {
+            let id = contientToutChamp[i].split(",");
+            console.log(document.getElementById(id[0]).length);
+            if (document.getElementById(id[0]).type === "number" && !regex.test(document.getElementById(id[0]).value) && document.getElementById(id[0]).innerHTML.length !== 0) {
+                tableauErreur.push(<p key={i}>{"Le champ " + document.getElementById("label" + id[0]).textContent + " contient des caractères alphanumériques. Veillez y remédier"}</p>);
+                erreurAlpha = true;
+            }
+
+            if (!document.getElementById("valeurVide" + id[0]).hidden) {
+                if (document.getElementById(id[0]).type === "checkbox" || document.getElementById(id[0]).type === "radio") {
+                    tableauErreur.push(<p key={i}>{"Le champ " + document.getElementById("label" + id[0]).textContent + " n'est pas coché. Veillez y remédier"}</p>);
+                } else {
+                    tableauErreur.push(<p key={i}>{"Le champ " + document.getElementById("label" + id[0]).textContent + " ne contient aucun critére. Veillez y remédier"}</p>);
+                }
+                erreurChampRequis = true;
+            }
+        }
+
+        if (erreurAlpha || erreurChampRequis) {
+            SetErreur(true);
+        } else {
+            SetErreur(false);
+        }
+
+        SetAffichageErreur(tableauErreur);
+        erreurAlpha = false;
+        erreurChampRequis = false;
+    }
+
+    function regroupe() {
+        donneeRequise();
+        verificationErreur();
+    }
 
     return (
         <>
+            {erreur && <div>
+                <h3>Résumé des erreurs</h3>
+                {affichageErreur.map((uneErreur) => (uneErreur))}
+            </div>}
+
             {information.length ?
                 (
                     information.map((donnee, i) => (
-                        <div key={i}>
-                            <div>
-                                <h1 id={donnee.id}>{donnee.title}</h1>
-                                {donnee.inputs.map((entree, j) => (
+                        < div key={i} >
+                            <h1 id={donnee.id}>{donnee.title}</h1>
+                            {
+                                donnee.inputs.map((entree, j) => (
                                     <div key={j}>
-                                        <label key={"label" + i}>{entree.label}</label>
-                                        {gestionChamp(entree)}
+                                        {gestionChamp(entree, i)}
                                     </div>
-
-                                ))}
-                            </div>
-                            <button> Soumettre</button>
-                        </div>
-
+                                ))
+                            }
+                        </div >
                     ))
                 ) : (<p>loading...</p>)
             }
+            <button onClick={regroupe}>Soumettre</button >
         </>
     );
 }
